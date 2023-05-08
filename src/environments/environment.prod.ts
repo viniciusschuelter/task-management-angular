55c8025a7fa8e71ee327bcbdd3db753046f92bbb
VInicius Schuelter <48526428+viniciusschuelter@users.noreply.github.com>
2023-05-08 15:57:36 -0300

    feat(task-management): task-management create enviroments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  urlBase: 'https://localhost:8080/api/v1'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  urlBase: 'https://localhost:8080/api/v1'
+};
